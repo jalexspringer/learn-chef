fe758648ff8e7d99f7fae88248c66001904d7994
Alex S <jalexspringer@gmail.com>
2017-04-08 14:37:30 -0400

    database tests

--- cookbooks/lamp/test/smoke/default/database_test.rb
+++ cookbooks/lamp/test/smoke/default/database_test.rb
@@ -4,15 +4,16 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe mysql_conf('/etc/mysql-default/my.cnf').params('mysqld') do
+  its('port') { should eq '3306' }
+  its('socket') { should eq '/run/mysql-default/mysqld.sock' }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port 3306 do
+  it { should be_listening }
+  its('protocols') { should include('tcp') }
+end
+
+describe command("mysql -h 127.0.0.1 -uroot -p fakerootpassword -s -e 'show databases;'") do
+  its('stdout') { should match(/mysql/) }
 end
