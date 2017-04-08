09328a7645b65338fbe2cba19015fd25bd3cbfa8
vagrant <vagrant@vagrant.vm>
2017-04-08 17:15:03 +0000

    initial commit

---
+++ cookbooks/lamp/metadata.rb
@@ -0,0 +1,22 @@
+name 'lamp'
+maintainer 'Alex S'
+maintainer_email 'jalexspringer@gmail.com'
+license 'All Rights Reserved'
+description 'Installs/Configures lamp'
+long_description 'Installs/Configures lamp'
+version '0.1.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+issues_url 'https://github.com/jalexspringer/lamp/issues'
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+source_url 'https://github.com/jalexspringer/lamp'
+
+depends 'httpd', '~> 0.4'
