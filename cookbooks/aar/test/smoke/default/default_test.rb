ad72976ffb8ebaf74180dbcb33aa3b53925e2931
Alex S <jalexspringer@gmail.com>
2017-04-09 10:51:00 -0400

    added tests to aar

--- cookbooks/aar/test/smoke/default/default_test.rb
+++ cookbooks/aar/test/smoke/default/default_test.rb
@@ -5,14 +5,33 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe mysql_conf('/etc/mysql-default/my.cnf').params('mysqld') do
+  its('port') { should eq '3306' }
+  its('socket') { should eq '/run/mysql-default/mysqld.sock' }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port 3306 do
+  it { should be_listening }
+  its('protocols') { should include('tcp') }
+end
+
+describe command("mysql -h 127.0.0.1 -uroot -pfakerootpassword -s -e 'show databases;'") do
+  its('stdout') { should match(/mysql/) }
+end
+
+describe package 'apache2' do
+  it { should be_installed }
+end
+
+describe service 'apache2-default' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe command 'wget -qSO- --spider localhost' do
+  its('stderr') { should match %r{HTTP/1\.1 200 OK} }
+end
+
+describe port 80 do
+  it { should be_listening }
 end
