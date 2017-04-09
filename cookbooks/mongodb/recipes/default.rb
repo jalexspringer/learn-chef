54ac7311319eed7daf05acb26837315b382d4490
AlexS <jalexspringer@gmail.com>
2017-04-09 15:33:51 +0000

    mongodb cookbook firing for 64-bit

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -2,4 +2,20 @@
 # Cookbook:: mongodb
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Alex Springer, All Rights Reserved.
+
+cookbook_file '/etc/yum.repos.d/mongodb.repo' do
+  source 'mongodb.repo'
+end
+
+package 'mongodb-org'
+
+service 'mongod' do
+  action [:enable, :start]
+end
+
+bash 'check_start_on_reboot' do
+  code <<-EOH
+    sudo chkconfig mongod on
+  EOH
+end
