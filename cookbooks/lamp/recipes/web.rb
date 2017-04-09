d87baa554994184a0acff0a66cd67245f9d12b94
Alex S <jalexspringer@gmail.com>
2017-04-08 21:00:47 -0400

    c

--- cookbooks/lamp/recipes/web.rb
+++ cookbooks/lamp/recipes/web.rb
@@ -16,17 +16,15 @@ end
 
 # Install apache and start
 httpd_service 'default' do
-  mpm 'prefork'
   action [:create, :start]
   subscribes :restart, 'httpd_config[default]'
 end
 
-# PHP Stuff Starts Here
-httpd_module 'php5' do
+httpd_module 'wsgi' do
   instance 'default'
 end
 
-package 'php5-mysql' do
-  action :install
-  notifies :restart, 'httpd_service[default]'
-end
+# package 'php5-mysql' do
+#   action :install
+#   notifies :restart, 'httpd_service[default]'
+# end
