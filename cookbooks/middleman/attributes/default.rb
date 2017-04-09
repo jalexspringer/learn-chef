e40b512c0cf8d82571d123616cbd3fe71e86567f
Alex S <jalexspringer@gmail.com>
2017-04-09 12:41:59 -0400

    first pass ruby

---
+++ cookbooks/middleman/attributes/default.rb
@@ -0,0 +1,2 @@
+default['middleman']['home']          = '/home/middleman'
+default['middleman']['install_dir']   = '/home/middleman/middleman-blog'
--- cookbooks/middleman/recipes/ruby.rb
+++ cookbooks/middleman/recipes/ruby.rb
@@ -3,7 +3,32 @@
 # Recipe:: ruby
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'runit'
 
-package [ 'git', 'ruby' ]
+user 'middleman' do
+  comment 'middleman ruby user'
+  home node['middleman']['home']
+  gid 'users'
+  action :create
+end
 
-git ''
+git node['middleman']['install_dir'] do
+  repository 'https://github.com/learnchef/middleman-blog.git' 
+  reference 'master'
+  user 'middleman'
+  group 'users'
+  action :sync
+end
+
+ruby_runtime 'middleman' do
+  version '2.1'
+end
+
+bundle_install node['middleman']['install_dir'] do
+  deployment true
+  ruby 'middleman'
+  user 'middleman'
+end
+
+runit_service "thin"
+
