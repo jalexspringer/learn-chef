ab3bc60f9c34a6830856056e1e9a48152f0f717e
AlexS <jalexspringer@gmail.com>
2017-04-09 15:53:05 +0000

    started middleman

---
+++ cookbooks/middleman/recipes/ruby.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: middleman
+# Recipe:: ruby
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
---
+++ cookbooks/middleman/recipes/web.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: middleman
+# Recipe:: web
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
